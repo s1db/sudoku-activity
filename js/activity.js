bf14705abaac19ba7917277e2d32352022f29821
cosmicasymmetry <cosmicasymmetry@github.com>
2018-01-04 16:56:48 +0530

    title bug fix

--- js/activity.js
+++ js/activity.js
@@ -1,14 +1,14 @@
-define(["sugar-web/activity/activity"], function (activity) {
+define(["sugar-web/activity/activity"], function(activity) {
 
-	// Manipulate the DOM only when it is ready.
-	require(['domReady!'], function (doc) {
+  // Manipulate the DOM only when it is ready.
+  require(['domReady!'], function(doc) {
 
-		// Initialize the activity.
-		activity.setup();
-		var GoL = document.getElementById("Gol");
-		GoL.onclick = function() {
-			console.log("you clicked me!");
-		}
-	});
+    // Initialize the activity.
+    activity.setup();
+    var GoL = document.getElementById("Gol");
+    GoL.onclick = function() {
+      console.log("you clicked me!");
+    }
+  });
 
 });
